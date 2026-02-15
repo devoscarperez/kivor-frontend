31247b252764f1d8297aae80b6c21dd249bc455e
devoscarperez <144189730+devoscarperez@users.noreply.github.com>
2026-02-15 15:53:18 -0400

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,19 @@
+const API_BASE = "https://kivor.onrender.com";
+
+async function obtenerGananciasPorMes(mes) {
+    try {
+        const response = await fetch(`${API_BASE}/ganancias-por-mes?mes=${mes}`);
+        
+        if (!response.ok) {
+            throw new Error("Error al consultar la API");
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("Error:", error);
+        return null;
+    }
+}
  
