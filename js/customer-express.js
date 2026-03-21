1d7b2cb2cbe14239faadbafb51cd46cb9264af4e
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-03-21 12:54:58 -0300

    Update customer-express.js

--- js/customer-express.js
+++ js/customer-express.js
@@ -17,7 +17,8 @@ const STORAGE_KEY = "kivor_customer_express_draft";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    if (typeof API_BASE === "undefined") {
+    # if (typeof API_BASE === "undefined") {
+    if (!API_BASE) {    
         console.error("API_BASE not defined");
         showError("msg_connection_lost");
         return;
