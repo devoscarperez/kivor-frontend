52984c4a9ddfa9c81d460e3cd7e920fa49016b7f
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-03-25 08:00:07 -0300

    Update input-base.js

--- js/components/input-base.js
+++ js/components/input-base.js
@@ -1,1 +1,23 @@
+export function createInputBase({ id }) {
 
+    const input = document.getElementById(id);
+    const error = document.getElementById(`${id}-error`);
+
+    function setError(message) {
+        input.classList.add("input-invalid");
+        error.textContent = message;
+        error.classList.add("active");
+    }
+
+    function clearError() {
+        input.classList.remove("input-invalid");
+        error.textContent = "";
+        error.classList.remove("active");
+    }
+
+    return {
+        input,
+        setError,
+        clearError
+    };
+}
